0fc95347fbeacbb6a05feff9492a6ae9962af186
Helmut Konrad Fahrendholz <helmutus@outlook.com>
2022-12-02 11:28:07 +0000

    chore(setup): use utila quick installer

--- setup.py
+++ setup.py
@@ -8,41 +8,9 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import os
-import re
+import utila
 
-import setuptools
-
-ROOT = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(ROOT, 'README.md'), encoding='utf8') as fp:
-    README = fp.read()
-
-with open(os.path.join(ROOT, 'resinf/__init__.py'), encoding='utf8') as fp:
-    VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
-
-with open(os.path.join(ROOT, "requirements.txt"), encoding='utf8') as fp:
-    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
+PACKAGES = ['resinf']
 
 if __name__ == "__main__":
-    # allow setup.py to run from another directory
-    os.chdir(ROOT)
-    setuptools.setup(
-        author='Helmut Konrad Fahrendholz',
-        author_email='info@checkitweg.de',
-        description='small one',
-        install_requires=REQUIRES,
-        long_description=README,
-        name='resinf',
-        platforms='any',
-        url='https://dev.package.checkitweg.de/resinf',
-        version=VERSION,
-        zip_safe=False,  # create 'zip'-file if True. Don't do it!
-        classifiers=[
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-        ],
-        packages=[
-            'resinf',
-        ],
-    )
+    utila.install(__file__)
